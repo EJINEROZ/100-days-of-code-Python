ca7efddac473485d624d21be021972f95b19f4a9
Ejineroz <89276697+EJINEROZ@users.noreply.github.com>
2022-09-12 14:03:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+result1 = int(weight)
+result2 = float(height)
+
+result = result1 / result2 ** 2
+
+print(result)
+
+
+
+
+
+
+
+
